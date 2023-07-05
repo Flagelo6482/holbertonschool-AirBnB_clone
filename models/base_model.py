b82a5cb4f43de33ab397704b483d682ed01328e2
Flagelo6482 <frankln6482@gmail.com>
2023-07-05 08:27:31 -0700

    Feat: Update method save :rocker:

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         """Initialization constructor"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Returns a user-readable string"""
