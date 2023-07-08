364867c18e6e8e7701fa4eb105cd64b90e19379f
Flagelo6482 <frankln6482@gmail.com>
2023-07-08 07:08:36 -0700

    Feat: Update :rocket:

--- models/base_model.py
+++ models/base_model.py
@@ -14,16 +14,21 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Initialization constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = dt.datetime.now()
-        self.updated_at = self.created_at
-        if kwargs:
-            if '__clas__' in kwargs:
-                del kwargs['__class__']
-                for k, v in kwargs.items():
-                    date = '%Y-%m-%dT%H:%M:%S.%f'
-                    if k in ['created_at', 'updated_at']:
-                        v = dt.datetime.strptime(v, date)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.datetime.now()
+            self.updated_at = dt.datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k == 'id':
+                    self.id = v
+                elif k == 'created_at':
+                    self.created_at = dt.datetime.fromisoformat(v)
+                elif k == 'updated_at':
+                    self.updated_at = dt.datetime.fromisoformat(v)
+                else:
                     setattr(self, k, v)
 
     def __str__(self):
