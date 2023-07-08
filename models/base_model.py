846c9d038f5ad446a3f62395b6b74023757d25d5
Flagelo6482 <frankln6482@gmail.com>
2023-07-08 06:55:10 -0700

    Feat: Update files :rocket:

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,18 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Initialization constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = dt.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            if '__clas__' in kwargs:
+                del kwargs['__class__']
+                for k, v in kwargs.items():
+                    date = '%Y-%m-%dT%H:%M:%S.%f'
+                    if k in ['created_at', 'updated_at']:
+                        v = dt.datetime.strptime(v, date)
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a user-readable string"""
