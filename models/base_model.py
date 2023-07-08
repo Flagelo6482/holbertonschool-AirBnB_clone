7181fc0c4c2058af3ae77ad18497ef403e84ddf8
Flagelo6482 <frankln6482@gmail.com>
2023-07-07 17:41:28 -0700

    Feat: Update clase BaseModel :rocket:

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 """BaseModel"""
 
-import models
 import datetime as dt
 import uuid
 
@@ -19,36 +18,23 @@ class BaseModel:
         self.created_at = dt.datetime.now()
         self.updated_at = self.created_at
 
-        if kwargs:
-            class_name = kwargs.pop('__class__', None)
-            if class_name:
-                date = "%Y-%m-%dT%H:%M:%S.%f"
-                for key, value in kwargs.items():
-                    if key in ['created_at', 'updated_at']:
-                        value = dt.datetime.strptime(value, date)
-                    setattr(self, key, value)
-        if not kwargs:
-            models.storage.new(self)
-
     def __str__(self):
         """Returns a user-readable string"""
-        st_dic = vars(self)
-        return f"[{self.__class__.__name__}] ({self.id}) {st_dic}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
         Updates the updated_at public instance
         attribute with the current date and time
         """
-        self.updated_at = datetime.datetime.now()
-        models.storage.save()
+        self.updated_at = dt.datetime.now()
 
     def to_dict(self):
         """
         Return a dictionary with the set values
         """
-        dic = self.__dict__.copy()
-        dic['__class__'] = self.__class__.__name__
+        dic = self.__dict__
+        dic['__class__'] = type(self).__name__
         dic['created_at'] = self.created_at.isoformat()
         dic['updated_at'] = self.updated_at.isoformat()
         dic['id'] = self.id
